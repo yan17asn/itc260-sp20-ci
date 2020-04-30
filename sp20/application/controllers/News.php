9087b3776e264ce5e69809e2027cc18ca2e09bcf
yan17asn <248764378@qq.com>
2020-04-30 16:24:34 -0700

    Update News.php

--- sp20/application/controllers/News.php
+++ sp20/application/controllers/News.php
@@ -16,9 +16,9 @@ class News extends CI_Controller {
             $data['news'] = $this->news_model->get_news();
             $data['title'] = 'News archive';
     
-            //$this->load->view('templates/header', $data);
+            
             $this->load->view('news/index', $data);
-            //$this->load->view('templates/footer', $data);
+            
         }
 
         public function view($slug = NULL)
@@ -32,9 +32,9 @@ class News extends CI_Controller {
 
             $data['title'] = $data['news_item']['title'];
 
-            //$this->load->view('templates/header', $data);
+            
             $this->load->view('news/view', $data);
-            //$this->load->view('templates/footer', $data);
+            
         }
         public function create()
         {
@@ -48,17 +48,17 @@ class News extends CI_Controller {
 
             if ($this->form_validation->run() === FALSE)
             {
-                //$this->load->view('templates/header', $data);
+                
                 $this->load->view('news/create', $data);
-                //$this->load->view('templates/footer', $data);        
+                       
 
             }
             else
             {
                 $this->news_model->set_news();
-                //$this->load->view('templates/header', $data);
+                
                 $this->load->view('news/success', $data);
-                //$this->load->view('templates/footer', $data);
+                
             }
         }
 }
